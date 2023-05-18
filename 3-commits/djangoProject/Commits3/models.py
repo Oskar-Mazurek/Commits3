6a5b13b646b27b9e0b246371a43119da7e117fef
Oskar-Mazurek <168461@stud.prz.edu.pl>
2023-05-18 18:36:26 +0200

    2. commit: - utworzenie bazy danych(encja ogłoszenie)

--- 3-commits/djangoProject/Commits3/models.py
+++ 3-commits/djangoProject/Commits3/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from datetime import date, datetime, timedelta
+
 
 # Create your models here.
+class Ad(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+    description = models.TextField(max_length=2000, null=False, blank=False)
+    cost = models.FloatField(verbose_name='Cena', null=False, blank=False)
+    publicationDate = models.DateTimeField(default=datetime.now(), blank=True)
+    expirationDate = models.DateTimeField(default=(datetime.now() + timedelta(days=14)), blank=True)
+
+    def __str__(self):
+        return 'ID: ' + str(self.pk) + \
+               'Nazwa ogłoszenia: ' + str(self.name) + \
+               ' Cena: ' + str(self.cost) + \
+               ' Data publikacji: ' + str(self.publicationDate + timedelta(hours=2)) + \
+               ' Data przedawnienia: ' + str(self.expirationDate + timedelta(hours=2))
